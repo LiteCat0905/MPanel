53fe701debbb8b0b3f8ec700c84f318ade86b93c
LiteCat <littttt87@hotmail.com>
2023-02-15 16:03:39 +0800

    first commit

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,38 @@
+
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+    post: Object,
+})
+
+
+const name = computed(() => {
+    return props.post.name;
+})
+const id = computed(() => {
+    return props.post.id;
+})
+</script>
+
+
+
+<template>
+    <n-space vertical>
+        <n-collapse-item :title="name" :name="id">
+            <span>{{ props.post.body }}</span>
+            <!-- eslint-disable vue/valid-v-for -->
+            <br>
+            <n-tag v-for="item in post.tag" :bordered="false">#{{ item }}</n-tag>
+            <template #header-extra>
+                <span>{{ props.post.emoji }}</span>
+            </template>
+        </n-collapse-item>
+    </n-space>
+
+</template>
+
+<style>
+.n-tag{
+    margin-top: 10px;
+}
+</style>
